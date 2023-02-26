ded782438a24a80a389bcdacea2e8124a36b3b20
AMiro <aj.miro.b@gmail.com>
2023-02-26 11:17:53 -0600

    setItem and getItem working for localStorage; starting work with day.js

--- assets/script.js
+++ assets/script.js
@@ -2,38 +2,41 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-// Display the current date in the header of the page.
+// Display the current date in the header of the page. Get current time with day.js.
+
+
+$(".time-block").each(function () {
+    // `this` already targets time-block, so you don't need to traverse to closest
+    var timeBlock = $(this).attr("id");
+    var savedData = localStorage.getItem(timeBlock);
+    // var userInput = $(this).siblings(".description").val();
+    $(this).find(".description").val(savedData);
+});
+
+// Apply the past, present, or future class to each time block by comparing the time-block id to the current time.
+  // The id attribute of each time-block be used to conditionally add or remove 
+  // the past, present, and future classes.
+
+$(".time-block").each(function () {
+  
+});
+
+  
 
 // Add click lister events on the saveBtn. 
 var saveBtn = $(".saveBtn");
-saveBtn.click
 $(saveBtn).click(function() {
 // Get time-block id's. Use DOM traversal to get correct "hour-x" id is for each saveBtn.
 // `this` targets saveBtn in the function so it won't work outside the function:
-    var timeBlock = $(this).first(".time-block").attr("id");
-    var userInput = $(this).first(".description").val();
+    var timeBlock = $(this).closest(".time-block").attr("id");
+    console.log("timeBlock", timeBlock);
+    var userInput = $(this).siblings(".description").val();
+    console.log("userInput", userInput);
 // Set any userInput to localStorage 
 //  Use the id in the containing time-block as a key to save the user input in local storage. 
     localStorage.setItem(timeBlock, userInput);
 });
 
-// ref error:
-localStorage.getItem(timeBlock, userInput);
-
-// Get time-block id's. 
-
-// Use DOM traversal to get correct "hour-x" id is for each saveBtn.
-// `this` targets saveBtn in the function so you need to traverse to c 
-
-
-// Set any userInput to localStorage 
-//  Use the id in the containing time-block as a key to save the user input in local storage. 
-
-// Get current time with day.js.
-
-// Apply the past, present, or future class to each time block by comparing the time-block id to the current time.
-  // The id attribute of each time-block be used to conditionally add or remove 
-  // the past, present, and future classes.
 
 // Add code to get any user input that was saved in localStorage 
 // and set the values of the corresponding textarea elements. 
